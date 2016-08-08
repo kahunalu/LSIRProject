8c6f3ade93e824caeb4f690c8c0d06d06192b8ff
Luke Mclaren <luke.patrick.mclaren@gmail.com>
2016-08-08 13:45:56 -0700

    Removed unused code and fixed covNet

--- classifiers/theano_alexnet/preprocessing/generate_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python make_hkl.py
-python make_train_val_txt.py
-python make_labels.py
--- classifiers/theano_alexnet/preprocessing/generate_toy_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python make_hkl.py toy
-python make_train_val_txt.py
-python make_labels.py
